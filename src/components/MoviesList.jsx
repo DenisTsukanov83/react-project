b545db7c91eb5c5c961eb2db18c94abdd8d221c5
Denis Tsukanov <tsukanovdenis83@yandex.ru>
2024-02-29 22:15:22 +0400

    first commit

---
+++ src/components/MoviesList.jsx
@@ -0,0 +1,15 @@
+import {Movie} from './Movie';
+
+function MoviesList(props) {
+    const {movies = []} = props;
+    console.log(Movie)
+
+    return <div className='movies'>
+        {movies.length ? movies.map(movie => (
+                <Movie key={Movie.id}{...movie}/>
+            )) : <h5>Nothing found</h5>
+        }
+    </div>
+}
+
+export {MoviesList};
